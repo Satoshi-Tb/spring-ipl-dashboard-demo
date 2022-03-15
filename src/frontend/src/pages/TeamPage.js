0b08d8d8ebd8e17fb2e41cbf0ee7ec356f54d97d
Satoshi T <sulp91_fy@yahoo.co.jp>
2022-03-16 00:27:03 +0900

    円グラフ追加

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
+import { PieChart } from "react-minimal-pie-chart";
 
 import "./TeamPage.scss";
 
@@ -35,7 +36,20 @@ export const TeamPage = () => {
           <div className="team-name-section">
             <p className="team-name">{team.teamName}</p>
           </div>
-          <div className="win-loss-section">Wins / Losses</div>
+          <div className="win-loss-section">
+            Wins / Losses
+            <br />
+            <PieChart
+              data={[
+                {
+                  title: "Losses",
+                  value: team.totalMatches - team.totalWins,
+                  color: "#E15454",
+                },
+                { title: "Wins", value: team.totalWins, color: "#4da375" },
+              ]}
+            />
+          </div>
           <div className="match-detail-section">
             <h3>Latest Matches</h3>
             <MatchDetailCard teamName={teamName} match={team.matchs[0]} />
